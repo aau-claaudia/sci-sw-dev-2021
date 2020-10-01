bdae54eb9fc8370ba91c8abae00ce1b47c799f27
CelinePetersen <71002398+CelinePetersen@users.noreply.github.com>
2020-10-01 13:02:42 +0200

    Added explanations to bash script

--- combine_python_and_r_script.sh
+++ combine_python_and_r_script.sh
@@ -3,12 +3,18 @@
 # Version 1.0 
 # Date 09-24-2020
 
+# Extract ids from every file in the directory data/
 for file in data/*.json; \
 	do python3 print_kpthesaurus.py $file; \
 done > id_from_python.txt
 
+# Get filenames from every file in the directory data/. 
+# The first part of the pipeline lists the filenames of each file in data/.
+# The second part of the pipeline removes the.jason extension of the filenames
 ls data/ | sed 's/.json$//' > filename.txt  
 
+# Combine the two txt files
 pr -mtJS';' filename.txt id_from_python.txt > data_analyse.dat
 
+# Run the data analysis 
 Rscript analyse_and_visualize.r data_analyse.dat
