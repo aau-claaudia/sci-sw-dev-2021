0c4856b418c2eedf1022e6b6fe85cef64e0e809f
Søren Strandskov Sørensen <SStrandskov@h105.aau1x-2.klient.frb7h.site.aau.dk>
2021-09-28 09:30:35 +0200

    Added meaningful README file and modified bash script (glue.sh) with comments of internal behavior

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# sci-sw-dev-2021
-Repository for projects in the PhD course Tools for Scientific Software Development and Data Science
+#README
+
+## Written by Søren Strandskov Sørensen, soe@bio.aau.dk
+
+Included in this project is a bash script (glue.sh) intended for coupling a python (print_kpthesaurus.py) and R (analyse_and_visualize.r) script with the overall intend to analyze the distribution of used IDs of the European Court of Human Rights (ECoHR).
+
+The script is simply run by calling the following line in a bash shell:
+
+bash glue.sh
+
+This will output a file (plotinput.dat) with case numbers and associated IDs separated by a semicolon. This file is then taken as input to finally plot the distribution of IDs used by the ECoHR as plotinput.pdf.
+
+The code is tested on a macOS Big Sur 2017 Macbook Pro. BASH version 3.2.57, Python version 2.7.16, and R version 4.1.1.
--- glue.sh
+++ glue.sh
@@ -2,13 +2,13 @@
 
 ## BASH SCRIPT FOR COUPLING PYTHON AND R SCRIPT ##
 
-for j in data/*
+for j in data/* #Loop over all file names in data folder
 do
-    j2=${j::${#j}-5}
-    j2=${j2:5}
-    j3=$(python print_kpthesaurus.py ./$j)
-    totline="${j2};${j3}"
-    echo $totline >> plotinput.dat
+    j2=${j::${#j}-5} 				#Remove the last five symbols of the filenames, i.e. the ".json" extension
+    j2=${j2:5} 					#Remove all text preeceding the case number, i.e. data/
+    j3=$(python print_kpthesaurus.py ./$j) 	#Get the associated ID of case $j
+    totline="${j2};${j3}" 			#Combine the case number and IDs in a string and separated by a semicolon
+    echo $totline >> plotinput.dat		#Write output of python script to a new file (plotinput.dat)
 done 
 
-Rscript analyse_and_visualize.r plotinput.dat
+Rscript analyse_and_visualize.r plotinput.dat 	#Run R script with the freshly generated plotinput.dat
