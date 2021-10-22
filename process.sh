eaf34c69ea691e7433094940f7ee2e37179d5933
Olivier Pelgrin <olivier@cs.aau.dk>
2021-10-22 09:06:39 +0200

    extract the document ID of each data file

--- process.sh
+++ process.sh
@@ -1,2 +1,6 @@
-data_files=$(echo "$(pwd)"/data/*.json)
-echo "$data_files"
+data_files=$(ls ./data/*.json)
+
+for file in $data_files; do
+    name=$(echo "$file" | sed -E 's/\.\/data\/(.+).json/\1/')
+    echo "'"$name"'"
+done
