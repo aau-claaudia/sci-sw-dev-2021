25008c294e11f876754636e971acf47e8dcf830e
Olivier Pelgrin <olivier@cs.aau.dk>
2021-10-22 09:48:58 +0200

    store result of processing in .dat file

--- process.sh
+++ process.sh
@@ -1,6 +1,12 @@
+if [ -z "$1" ]; then
+    echo "Usage $0 output_filename"
+    exit
+fi
+
 data_files=$(ls ./data/*.json)
 
 for file in $data_files; do
+    ids=$(python3 ./print_kpthesaurus.py $file)
     name=$(echo "$file" | sed -E 's/\.\/data\/(.+).json/\1/')
-    echo "'"$name"'"
+    echo "$name;$ids" >> "$1.dat"
 done
