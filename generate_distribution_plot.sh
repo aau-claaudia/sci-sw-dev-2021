63f77cb84745a65e922481b74fa3d6badc773714
chsa_aau <chsa@es.aau.dk>
2021-10-30 17:21:46 +0200

    'generate_distribution_plot.sh' is a bash script to generate the distribution using the R script. It needs to be stored and run from the same directory as the data folder.
    It generates two files - distribution.txt as the output of the python script and distribution.pdf as output of the R script.

---
+++ generate_distribution_plot.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+touch ./python_out.txt
+for filename in ./data/*; do
+ file=${filename##*/}
+ output_python=$(python print_kpthesaurus.py data/$file 2>&1)
+ concat="${file%%.*};${output_python}"
+ echo $concat >> distribution.txt
+ done
+Rscript analyse_and_visualize.r distribution.txt
