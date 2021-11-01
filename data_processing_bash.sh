44e8d24015b499d92d4cebab38906d2993b7c7f0
YuriiIotov#4669 <yio@create.aau.dk>
2021-11-01 17:29:27 +0100

    adding user interface to enter a filename where to store the ID data and check if the filename is already exist

--- data_processing_bash.sh
+++ data_processing_bash.sh
@@ -1,7 +1,27 @@
 #!/bin/bash
-rm IDs.dat
-for filename in data/*.json; do
-	( echo $(basename --  ${filename%%.*}) | tr -d '\n' ; echo ";" | tr -d '\n' ; python print_kpthesaurus.py $filename ) >> IDs.dat
+echo "Please, enter filename where ID data will be extracted"
+                read IDfilename
+
+IDfilename=$IDfilename.dat
+echo "The data will be extracted to $IDfilename and ploted in ${IDfilename%%.*}.pdf"
+
+if [ -f "$IDfilename" ]; then
+	while true; do
+		read -p "The files $IDfilename and ${IDfilename%%.*}.pdf already exist in the current folder, Do you wish to overwrite them (y/n)?" yn
+	    		case $yn in
+			       	[Yy]* ) echo -n "" > "$IDfilename"; break;;
+       				[Nn]* ) echo "Please, enter another filename where ID data will be extracted"; \
+					read IDfilename; IDfilename=$IDfilename.dat; \
+					echo "The data will be extracted to $IDfilename and ploted in ${IDfilename%%.*}.pdf" ; break;;
+				* ) echo "Please answer yes or no.";
+			esac
+	done
+fi
+
+echo "Processing..."
+
+for JSONfilename in data/*.json; do
+( echo $(basename --  ${JSONfilename%%.*}) | tr -d '\n' ; echo ";" | tr -d '\n' ; python print_kpthesaurus.py $JSONfilename ) >> "$IDfilename"
 done
 
-Rscript analyse_and_visualize.r IDs.dat
+Rscript analyse_and_visualize.r "$IDfilename"
