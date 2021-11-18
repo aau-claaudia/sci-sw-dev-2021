6a7c800392d00f819279ee3329541c7231aab7f6
Esther.H.Kim <hki@cs.aau.dk>
2021-11-18 13:37:52 +0100

    revised for comment adding

--- print_files.py
+++ print_files.py
@@ -5,18 +5,12 @@ import sys
 
 
 file_names = os.listdir('./data')   # Read all files from /data folder.
-print(file_names)                   # Return
-
-
-
-
-
-
+print(file_names)                   # Return list of file names.
 
 def process(filename, pfield='kpthesaurus', pfield2='itemid'):
     with open(filename, 'r') as f:
         jsondata = json.load(f)
-        print(jsondata[pfield2], end=";")
+        print(jsondata[pfield2], end=";")   # For Rscript required format.
         print(jsondata[pfield])
 
 
@@ -24,7 +18,7 @@ if __name__ == '__main__':
     #parser = argparse.ArgumentParser(description='Process a single ECHR file.')
     #parser.add_argument('filename', type=str,
     #                    help='filename')
-
+    #
     #args = parser.parse_args()
 
     for i in file_names:
