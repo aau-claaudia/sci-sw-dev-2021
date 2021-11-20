0d7fdec6cd58d57715d3491a22b4fde9909d1631
Esther.H.Kim <hki@cs.aau.dk>
2021-11-20 14:34:03 +0100

    commit

--- print_files.py
+++ print_files.py
@@ -1,5 +1,5 @@
 import os
-import argparse
+#import argparse
 import json
 import sys
 
---
+++ print_list.py
@@ -0,0 +1,7 @@
+import os
+import json
+import sys
+
+
+file_names = os.listdir('./data')   # Read all files from /data folder.
+print(file_names)
