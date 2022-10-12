b5c992062a23666e786e1aca44f9972971ef401a
mbg <mbg@energy.aau.dk>
2022-10-12 11:43:50 +0200

    TASK COMPLETE: added for-loop to runme.sh to analyze all data

--- runme.sh
+++ runme.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
+
 touch ID_data.txt 	# create file for storing ID's
 > ID_data.txt 		# clear the file for any content
 
-#python print_kpthesaurus.py data/001-100865.json >> ID_data.txt
-output=$((python print_kpthesaurus.py data/001-100865.json) 2>&1)
-filename="001-100865;"
-echo "$filename$output" >> ID_data.txt
-echo "$filename$output" >> ID_data.txt
+for path in data/*.json
+do
+	output=$((python print_kpthesaurus.py "$path") 2>&1)
+	filename="${path##*/}"; filename="${filename%.*}"
+	echo "$filename$output" >> ID_data.txt
+#	echo "$filename;$output"
+done
 
 Rscript analyse_and_visualize.r ID_data.txt
