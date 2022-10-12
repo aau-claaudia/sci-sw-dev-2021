824308ce3f7d17fb53290d8aee993561f87dd1ee
mbg <mbg@energy.aau.dk>
2022-10-12 10:35:38 +0200

    runme.sh can now run the Python-script once and save filename+output to ID_data.txt

--- runme.sh
+++ runme.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
-
-
 touch ID_data.txt 	# create file for storing ID's
 > ID_data.txt 		# clear the file for any content
+
+#python print_kpthesaurus.py data/001-100865.json >> ID_data.txt
+output=$((python print_kpthesaurus.py data/001-100865.json) 2>&1)
+filename="001-100865;"
+echo "$filename$output" >> ID_data.txt
 
