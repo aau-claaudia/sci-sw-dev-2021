62b90565ecc4dbb6cceb854d41390a581699af0d
PJGilmw <pijo@plan.aau.dk>
2022-10-21 15:52:18 +0200

    The script now generates the intermediary output file

--- script_process.sh
+++ script_process.sh
@@ -1,17 +1,14 @@
-  
+touch output_readable_for_R.json
+> output_readable_for_R.json 
 
-total_output=$(
+
 for file in ./data/*.json
 
 do	
         docid=${file##*/}
         docid=${docid%.*}
-        
-	legal_ID= python print_kpthesaurus.py $file
-	var_toprint ="${docid};${legal_ID}" 
-	
-	echo ${var_toprint// /}
-	
-done)
+	legal_ID=$(python print_kpthesaurus.py $file)    
+	echo -e "${docid};${legal_ID}" >> output_readable_for_R.json
+done
 
-echo $total_output > output_readable_for_R.json
+
