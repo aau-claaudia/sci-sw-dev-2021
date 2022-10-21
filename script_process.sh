35ae8a68678abcb90d379e0506d0b990eae68a8d
PJGilmw <pijo@plan.aau.dk>
2022-10-21 23:27:06 +0200

    the script now works as wanted

--- script_process.sh
+++ script_process.sh
@@ -1,15 +1,15 @@
-if [ -f output_readable_for_R.json] && [ -s output_readable_for_R.json ]  #If the file with pre-processed data already exists and is not empty
+if [ -f output_frequency.json ] && [ -s output_frequency.json ]  #If the file with pre-processed data already exists and is not empty
 
     then  # We visualize directly the content
     echo "Data has already been pre-processed"
-    Rscript analyse_and_print.r  output_readable_for_R.json 
+    Rscript analyse_and_visualize.r  output_frequency.json 
     
     else # We pre-process the data with the python script and visualize it afterwards
 	
 	echo "Data is being processed"
 
-        touch output_readable_for_R.json 	
-	> output_readable_for_R.json
+        touch output_frequency.json 	
+	> output_frequency.json
 
 
 	for file in ./data/*.json
@@ -18,10 +18,10 @@ if [ -f output_readable_for_R.json] && [ -s output_readable_for_R.json ]  #If th
         	docid=${file##*/}
        	        docid=${docid%.*}
         	legal_ID=$(python print_kpthesaurus.py $file)
-       		echo -e "${docid};${legal_ID}" >> output_readable_for_R.json
+       		echo -e "${docid};${legal_ID}" >> output_frequency.json
 	done
 
-	Rscript analyse_and_print.r  output_readable_for_R.json
+	Rscript analyse_and_visualize.r  output_frequency.json
    
 
  fi
